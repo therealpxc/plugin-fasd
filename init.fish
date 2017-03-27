ba7be149783d7366b25cd4578e423a7e94d61772
Patrick Callahan <pxcallahan@gmail.com>
2017-03-26 18:43:37 -0700

    try the --on-variable $PWD fix and see how it feels

--- init.fish
+++ init.fish
@@ -1,8 +1,8 @@
 # Detect fasd
 if type -q fasd
   # Hook into fish preexec event
-  function __fasd_run -e fish_preexec
-    command fasd --proc (command fasd --sanitize "$argv" | tr -s ' ' \n) > "/dev/null" 2>&1 &
+  function __fasd_run -v PWD
+    command fasd --proc (command fasd --sanitize "$argv" | tr -s ' ' \n) > "/dev/null" 2>&1
   end
 else
   echo "🍒  Please install 'fasd' first!"
